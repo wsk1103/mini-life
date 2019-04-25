8f43459f1a73b32ab6f3b109072c1c7648e7b337
wsk <1261709167@qq.com>
2019-04-25 21:22:24 +0800

    update

--- src/main/java/com/wsk/movie/controller/admin/FindUserController.java
+++ src/main/java/com/wsk/movie/controller/admin/FindUserController.java
@@ -42,9 +42,6 @@ public class FindUserController {
     private AdminRepository adminRepository;
 
     @Autowired
-    private ReportRepository reportRepository;
-
-    @Autowired
     private AdminActionMapper adminActionMapper;
 
     @Autowired
--- src/main/java/com/wsk/movie/error/LoginErrorResolver.java
+++ src/main/java/com/wsk/movie/error/LoginErrorResolver.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * @DESCRIPTION :
+ * @DESCRIPTION : 全局异常处理
  * @AUTHOR : WuShukai1103
  * @TIME : 2018/1/19  20:46
  */
